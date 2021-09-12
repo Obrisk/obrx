6cb3123d72f3c9ca4c5730404763fabe50ec634e
elkd <el@obrisk.com>
2021-09-12 11:35:42 +0800

    added wechat backend to save the user to database

--- wechat_bot/views.py
+++ wechat_bot/views.py
@@ -1,29 +1,30 @@
 #wechat chatbot
 import logging
 import redis
+from urllib.parse import urlparse
 from django.conf import settings
 from django.http import HttpResponse
-from werobot import WeRoBot
 
+from werobot import WeRoBot
 from werobot.session.redisstorage import RedisStorage
 from obrx.settings.base import env
-from users.models import User, WechatUser
-from utils.wx_config import get_access_token, get_user_info
-from users.tasks import upload_image
+from utils.wx_config import get_access_token
+from users.models import User
+from wechat_bot.processing import handle_wechat_user, upload_classifieds
 
 
-#db = redis.Redis(settings.REDIS_URL)
-#correct way is:
-#db = redis.Redis(host=env('REDIS_HOST'), port=6379)
-#session_storage = RedisStorage(db, prefix="wxbot_")
+url = urlparse(settings.REDIS_URL)
+db = redis.Redis(host=url.hostname, port=6379)
+session_storage = RedisStorage(db, prefix="wxbot_")
 wxbot = WeRoBot(
             token=env('WXBOT_TOKEN'),
             app_id=env('WECHAT_APPID'),
             app_secret=env('WECHAT_APPSECRET'),
-            enable_session=True
-            #session_storage=session_storage
+            enable_session=True,
+            session_storage=session_storage
         )
-client=wxbot.client
+
+#client=wxbot.client
 
 
 @wxbot.handler
@@ -34,52 +35,38 @@ def hello(message):
 @wxbot.text
 def handle_text(message, session):
     userid = message.source
-    logging.error(f'Obrx server: ID: {userid}')
-    user = User.objects.filter(
-            wechat_openid=userid
-        )
-    if user.count() == 0:
-        wechat_user = WechatUser.objects.filter(
-                wechat_openid=userid
-            )
-        if wechat_user.count() == 0:
-
-            token = get_access_token()
-            if token is None:
-                logging.error('Wxbot failed to get access token')
-                return ''
-
-            user_info = get_user_info(token, userid)
-            if user_info is None:
-                logging.error('Wxbot failed to get user info')
-                return ''
-
-            picture = user_info['headimgurl']
-            thumbnail = picture[:-3] + '64'
-            full_image = picture[:-3] + '0'
-
-            th, mid, full = upload_image(
-               user_info['nickname'], thumbnail, picture, full_image
-            )
-
-            WechatUser.objects.create(
-                name=user_info['nickname'],
-                wechat_openid=user_info['openid'],
-                thumbnail = th,
-                picture = mid,
-                org_picture = full,
-                gender=user_info['sex'],
-                city=user_info['city'].encode('iso8859-1').decode('utf-8'),
-                province=user_info['province'].encode('iso8859-1').decode('utf-8'),
-                country=user_info['country'].encode('iso8859-1').decode('utf-8')
-            )
-
+    handle_wechat_user(userid)
     return ''
 
 
 @wxbot.image
 def handle_img(message, session):
     img = message.img
+    handle_wechat_user(userid)
+
+    user = User.objects.filter(
+            wechat_openid=userid
+        )
+    if user.count() > 0:
+        upload_classifieds(message, user.first())
+    return ''
+
+
+@wxbot.subscribe
+def handle_subscribe(message):
+    return "Thanks for following us. Send photos of your stuff for sale here & we'll upload for you😊" #noqa
+
+
+@wxbot.pic_photo_or_album
+def handle_photo_or_album(message):
+    userid = message.source
+    handle_wechat_user(userid)
+
+    user = User.objects.filter(
+            wechat_openid=userid
+        )
+    if user.count() > 0:
+        upload_classifieds(message, user.first())
     return ''
 
 
@@ -93,7 +80,7 @@ def set_custom_menu(request):
 
     try:
         response = requests.get(
-            url=f"https://api.weixin.qq.com/cgi-bin/menu/create?access_token={token}",
+            url=f"https://api.weixin.qq.com/cgi-bin/menu/create?access_token={token}", #noqa
             params={
                 "button": [
                     {
